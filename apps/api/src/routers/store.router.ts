e60c81f201316efb2f91e017b8a3ad06efe75ee4
bimobaskoro <bimoappbaskoro@gmail.com>
2024-07-23 13:36:27 +0700

    fead: FIN-60

--- apps/api/src/routers/store.router.ts
+++ apps/api/src/routers/store.router.ts
@@ -14,9 +14,9 @@ export class StoreRouter {
   private initializeRoutes(): void {
     this.router.post('/create', this.storeController.createStore);
     this.router.get('/', this.storeController.getStoresAll);
-    this.router.get('/:id', this.storeController.getStoreByStoreId);
     this.router.delete('/delete/:id', this.storeController.softDeleteStore);
     this.router.get('/available-store', this.storeController.availableStores);
+    this.router.get('/:id', this.storeController.getStoreByStoreId);
   }
 
   getRouter(): Router {
