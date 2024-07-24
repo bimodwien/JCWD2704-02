a91563f87085cfe4195ccbe46c4e5f116e3fa64b
bimobaskoro <bimoappbaskoro@gmail.com>
2024-07-24 21:01:37 +0700

    fead: assign store

--- apps/web/src/app/dashboard/admin/edit/[id]/page.tsx
+++ apps/web/src/app/dashboard/admin/edit/[id]/page.tsx
@@ -6,6 +6,7 @@ import * as Yup from 'yup';
 import { axiosInstance } from '@/lib/axios';
 import { AxiosError } from 'axios';
 import { useRouter } from 'next/navigation';
+import { availableStores } from '@/helpers/fetchStore';
 
 const EditAdmin = ({ params }: { params: { id: string } }) => {
   const router = useRouter();
@@ -14,7 +15,10 @@ const EditAdmin = ({ params }: { params: { id: string } }) => {
     name: '',
     email: '',
     password: '',
+    storeId: '', // Menambahkan storeId
   });
+  const [stores, setStores] = useState<any[]>([]); // State untuk daftar toko
+
   useEffect(() => {
     async function fetchDetail() {
       try {
@@ -24,14 +28,29 @@ const EditAdmin = ({ params }: { params: { id: string } }) => {
           name: dataAdmin.name,
           email: dataAdmin.email,
           password: dataAdmin.password,
+          storeId: dataAdmin.storeId || '', // Menambahkan storeId jika ada
         });
       } catch (error) {
-        if (error instanceof AxiosError) throw error.response?.data?.message;
-        else if (error instanceof Error) console.log(error.message);
+        if (error instanceof AxiosError)
+          console.error(
+            'Error fetching admin details:',
+            error.response?.data?.message,
+          );
+        else if (error instanceof Error) console.error('Error:', error.message);
       }
     }
+
     fetchDetail();
   }, [id]);
+
+  useEffect(() => {
+    async function fetchStores() {
+      await availableStores(setStores);
+    }
+
+    fetchStores();
+  }, []);
+
   const formik = useFormik({
     initialValues,
     enableReinitialize: true,
@@ -39,6 +58,7 @@ const EditAdmin = ({ params }: { params: { id: string } }) => {
       name: Yup.string().required('Nama wajib diisi').min(5),
       email: Yup.string().required('Email wajib diisi').email(),
       password: Yup.string().required('Password wajib diisi').min(8),
+      storeId: Yup.string().required('Store Location wajib diisi'), // Validasi untuk storeId
     }),
     onSubmit: async (values) => {
       try {
@@ -95,7 +115,7 @@ const EditAdmin = ({ params }: { params: { id: string } }) => {
                     htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900"
                   >
-                    email
+                    Email
                   </label>
                   <input
                     type="text"
@@ -105,7 +125,7 @@ const EditAdmin = ({ params }: { params: { id: string } }) => {
                     value={formik.values.email}
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
-                  />{' '}
+                  />
                   {formik.touched.email && formik.errors.email ? (
                     <div className="text-red-600 text-sm">
                       {formik.errors.email}
@@ -114,7 +134,7 @@ const EditAdmin = ({ params }: { params: { id: string } }) => {
                 </div>
                 <div>
                   <label
-                    htmlFor=""
+                    htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900"
                   >
                     Password
@@ -136,23 +156,36 @@ const EditAdmin = ({ params }: { params: { id: string } }) => {
                 </div>
                 <div>
                   <label
-                    htmlFor=""
+                    htmlFor="storeId"
                     className="block mb-2 text-sm font-medium text-gray-900"
                   >
                     Store Location
                   </label>
                   <select
-                    name=""
-                    id=""
+                    name="storeId"
+                    id="storeId"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                    value={formik.values.storeId}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                   >
                     <option value="">Select a location</option>
+                    {stores.map((store) => (
+                      <option key={store.id} value={store.id}>
+                        {store.name}
+                      </option>
+                    ))}
                   </select>
+                  {formik.touched.storeId && formik.errors.storeId ? (
+                    <div className="text-red-600 text-sm">
+                      {formik.errors.storeId}
+                    </div>
+                  ) : null}
                 </div>
                 <div className="flex items-center gap-5 pt-5">
                   <button
                     type="submit"
-                    className=" text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    className="text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   >
                     Edit
                   </button>
